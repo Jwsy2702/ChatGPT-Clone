8aad786923442a66784a9a988eb259c67711c2c9
Jwsy2702 <jonathanwsy@hotmail.com>
2023-12-27 21:16:59 +0800

    Chatgpt clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,158 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import gptLogo from "./assets/chatgpt.svg";
+import addBtn from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
+import home from "./assets/home.svg";
+import saved from "./assets/bookmark.svg";
+import upgrade from "./assets/rocket.svg";
+import sendBtn from "./assets/send.svg";
+import userIcon from "./assets/user-icon.jpg";
+import gptImgLogo from "./assets/chatgptLogo.svg";
+import { sendMsgToOpenAI } from "./openai";
+import { useRef, useState, useEffect } from "react";
 
 function App() {
+  const msgEnd = useRef(null);
+
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([
+    {
+      text: "Hi, I am ChatGPT. How can I help you?",
+      url: null,
+      isBot: true,
+    },
+  ]);
+
+  //for auto scroll, only when new message is added
+  useEffect(() => {
+    msgEnd.current.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  const handleSend = async () => {
+    const text = input;
+    setInput("");
+    setMessages([...messages, { text, isBot: false }]);
+    const [msg, url] = await sendMsgToOpenAI(input);
+    setMessages([
+      ...messages,
+      { text, isBot: false },
+      { text: msg, url: url, isBot: true },
+    ]);
+  };
+
+  //when user presses enter
+  const handleEnter = async (e) => {
+    if (e.key === "Enter") {
+      await handleSend();
+    }
+  };
+
+  //when user clicks on what is programming or how to use an api
+  const handleQuery = async (e) => {
+    const text = e.target.value;
+    setMessages([...messages, { text, isBot: false }]);
+    const msg = await sendMsgToOpenAI(input);
+    setMessages([
+      ...messages,
+      { text, isBot: false },
+      { text: msg, isBot: true },
+    ]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sideBar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src={gptLogo} alt="Logo" className="logo" />
+            <span className="brand">ChatGPT</span>
+          </div>
+          <button
+            className="midBtn"
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            <img src={addBtn} alt="new chat" className="addBtn" />
+            New Chat
+          </button>
+          <div className="upperSideBottom">
+            <button
+              className="query"
+              onClick={handleQuery}
+              value={"What is Programming?"}
+            >
+              <img src={msgIcon} alt="Query" />
+              What is Programming?
+            </button>
+            <button
+              className="query"
+              onClick={handleQuery}
+              value={"How to use an API?"}
+            >
+              <img src={msgIcon} alt="Query" />
+              How to use an API ?
+            </button>
+          </div>
+        </div>
+        <div className="lowerSide">
+          <div className="listItems">
+            <img src={home} alt="Home" className="listItemsImg" />
+            Home
+          </div>
+          <div className="listItems">
+            <img src={saved} alt="Saved" className="listItemsImg" />
+            Saved
+          </div>
+          <div className="listItems">
+            <img src={upgrade} alt="Upgrade" className="listItemsImg" />
+            Upgrade to Pro
+          </div>
+        </div>
+      </div>
+
+      <div className="main">
+        <div className="chats">
+          {messages.map((message, i) => (
+            <div key={i}>
+              <div className={message.isBot ? "chat bot" : "chat"}>
+                <img
+                  className="chatImg"
+                  src={message.isBot ? gptImgLogo : userIcon}
+                  alt="user"
+                />
+                <p className="txt">{message.text}</p>
+              </div>
+              {message.url && (
+                <div className="chat bot">
+                  <img className="resultImg" src={message.url} alt="user" />
+                </div>
+              )}
+            </div>
+          ))}
+          <div ref={msgEnd} />
+        </div>
+        <div className="chatFooter">
+          <div className="inp">
+            <input
+              type="text"
+              placeholder="Send a message"
+              value={input}
+              onKeyDown={handleEnter}
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+            />
+            <button className="send" onClick={handleSend}>
+              <img src={sendBtn} alt="Send" />
+            </button>
+          </div>
+          <p>
+            ChatGpt may produce inaccurate information about people, places, or
+            facts. ChatGPT Version 1
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/openai.js
@@ -0,0 +1,31 @@
+import OpenAI from "openai";
+// const apiUrl = "https://api.openai.com/v1/completions";
+const openai = new OpenAI({
+  apiKey: "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",
+  dangerouslyAllowBrowser: true,
+});
+
+export async function sendMsgToOpenAI(message) {
+  const hasImage = message.toLowerCase().includes("image");
+  let image = null;
+  if (hasImage) {
+    image = await openai.images.generate({
+      model: "dall-e-3",
+      prompt: message,
+      response_format: "url",
+      style: "natural",
+    });
+
+    message = message.replace("image", "describe");
+  }
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: message }],
+    max_tokens: 100, //characters
+    temperature: 0.7, //number between 0 and 2, higher values make output more random, lower values make it more focused and deterministic
+    frequency_penalty: 0,
+    presence_penalty: 0, //Number between -2 and 2, where negative numbers encourage the model to talk about new topics, and positive numbers encourage the model to respond similarly to previous statements.
+  });
+  return [completion.choices[0].message.content, image.data[0].url || null];
+}
